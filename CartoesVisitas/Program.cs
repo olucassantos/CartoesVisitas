a168c12da6b9159ece8cd789eeb842215137b2c5
Lucas Anjos <oargus.g@gmail.com>
2023-10-24 21:23:54 -0300

    Primeiro commit

---
+++ CartoesVisitas/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+/*
+    Criar um programa para escrever um cartão
+    de visita. O programa deve mostrar o nome,
+    profissão, email, descrição e telefone.
+*/
+
+// Cria as variáveis para armazenar os valores
+string? nome;
+string? profissao;
+string? telefone;
+string? email;
+string? descricao;
+
+// Solicita as informações do cartão
+Console.WriteLine("Qual o seu nome?");
+nome = Console.ReadLine();
+
+Console.WriteLine("Qual sua profissão?");
+profissao = Console.ReadLine();
+
+Console.WriteLine("Qual seu telefone?");
+telefone = Console.ReadLine();
+
+Console.WriteLine("Qual seu email?");
+email = Console.ReadLine();
+
+Console.WriteLine("Qual sua mensagem?");
+descricao = Console.ReadLine();
+
+// Escreve o cartão de visita
+Console.WriteLine("xxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+Console.WriteLine("x          "+ nome +"       x");
+Console.WriteLine("x  "+ profissao +"          x");
+Console.WriteLine("x  "+ telefone +"           x");
+Console.WriteLine("x  "+ email +"              x");
+Console.WriteLine("x                           x");
+Console.WriteLine("x  "+ descricao +"          x");
+Console.WriteLine("x                           x");
+Console.WriteLine("xxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
---
+++ README.md
@@ -0,0 +1,22 @@
+# Como criar e enviar um repositório para o GitHub?
+
+
+### 1 - Abrir a pasta do projeto no console.
+
+### 2 - Inicializar o repositório git:
+```
+    git init
+```
+
+### 3 - Configurar nome e email:
+```
+    git config user.name "Lucas Anjos"
+    git config user.email "oargus.g@gmail.com"
+```
+
+### 4 - Criar o primeiro commit
+
+```
+    git add .
+    git commit -am "Primeiro commit"
+```
